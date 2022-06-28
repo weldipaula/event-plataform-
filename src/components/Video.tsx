4173af1a78288d6312af4a7cea157bcf990b8a57
weldipaula <weldipaula@hotmail.com>
2022-06-27 22:09:42 -0300

    adicionado dados dinamicos ao video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,14 +1,61 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css'
-export function Video () {
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string
+    videoId: string
+    description: string
+    teacher: {
+      bio: string
+      avatarURL: string
+      name: string
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug : string
+}
+
+export function Video (props : VideoProps) {
+  const {data} = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
+  if(!data) {
+    return (
+      <div className="flex-1">
+        <p>carregando ...</p>
+      </div>
+    )
+  }
   return (
     <div className="flex-1 ">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+        <div className="h-full w-full max-w-[1000px] max-h-[80vh] aspect-video">
           <Player>
-            <Youtube videoId="SO4-izct7Mc"/>
+            <Youtube videoId={data.lesson.videoId}/>
             <DefaultUi />
           </Player>
         </div>
@@ -17,17 +64,21 @@ export function Video () {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="description flex-1">
-            <h1 className="text-2xl font-bold">Aula 01 - Criando o projeto e realizando o setup inicial</h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.</p>
+            <h1 className="text-2xl font-bold">
+            {data.lesson.title}
+            </h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+            {data.lesson.description}
+            </p>
             <div className="flex items-center gap-4 mt-6">
               <img 
-                src="https://github.com/weldipaula.png" 
+                src={data.lesson.teacher.avatarURL} 
                 alt="imagem do github" 
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
               />
               <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">Wellyngton de Paula</strong>
-                <span className="text-gray-200 text-sm block">Professor</span>
+                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
 
@@ -45,7 +96,7 @@ export function Video () {
         </div>
         <div className="gap-8 mt-20 grid grid-cols-2">
           <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
-            <div className="bg-green-700 h-full g-6 px-4 flex items-center">
+            <div className="bg-green-700 g-6 px-4 flex items-center">
               <FileArrowDown size={40}/>
             </div>
             <div className="py-6 leading-relaxed">
@@ -55,12 +106,12 @@ export function Video () {
                 Acesse o material complementar para acelerar o seu desenvolvimento
               </p>
             </div>
-            <div className="h-full p-6 flex items-center">
+            <div className="p-6 flex items-center">
               <CaretRight size={24}/>
             </div>
           </a>
           <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
-            <div className="bg-green-700 h-full g-6 px-4 flex items-center">
+            <div className="bg-green-700 g-6 px-4 flex items-center">
               <FileArrowDown size={40}/>
             </div>
             <div className="py-6 leading-relaxed">
@@ -70,7 +121,7 @@ export function Video () {
                 Acesse o material complementar para acelerar o seu desenvolvimento
               </p>
             </div>
-            <div className="h-full p-6 flex items-center">
+            <div className="p-6 flex items-center">
               <CaretRight size={24}/>
             </div>
           </a>
